af97b48f4c7f3edb547a5a9fd2ebef7ed0f5a1c4
sbyim <ysb06@hotmail.com>
2022-05-02 10:35:21 +0900

    Add functions
    New, Open, Save Dataframe

--- statsu/actions/file.py
+++ statsu/actions/file.py
@@ -1,6 +1,10 @@
-from statsu.actions import MenuActionGroup, MenuAction
-from statsu.widgets.pgui_window import PguiWindow
 import logging
+
+from pandas import DataFrame
+from PyQt5.QtWidgets import QFileDialog
+from statsu.actions import MenuAction, MenuActionGroup
+from statsu.widgets.pgui_window import PguiWindow
+from os.path import basename
 
 logger = logging.getLogger(__name__)
 
@@ -13,12 +17,26 @@ class FileActionGroup(MenuActionGroup):
             MenuAction('New Dataframe', parent, self.create_new_doc,'Ctrl+N'),
             MenuAction('Open File...', parent, parent.import_dialog,'Ctrl+O'),
             MenuAction('Switch Dataframe...', parent, self.open_navigator,'Ctrl+O'),
+            MenuAction('Save As...', parent, self.save_dialog,'Ctrl+S'),
             MenuAction('Quit', parent, parent.close,'Ctrl+Q'),
         ]
     
     def create_new_doc(self):
-        logger.info('New!!!')
-        print(self.parent.get_dataframes())
+        self.parent.store.add_dataframe(DataFrame({'0': [0, 0], '1': [0, 0]}))
 
     def open_navigator(self):
         self.parent.navigator.show()
+    
+    def save_dialog(self):
+        dialog = QFileDialog(parent=self.parent)
+        pgdf = self.parent.store.selected_pgdf
+        path, _ = dialog.getSaveFileName(directory=pgdf.name, filter='Excel Files (*.xlsx);;CSV files (*.csv)')
+
+        if path == False:
+            return
+        elif path.endswith('.xlsx'):
+            pgdf.df.to_excel(path)
+        elif path.endswith('csv'):
+            pgdf.df.to_csv(path, index=False)
+        
+        pgdf.name = basename(path)
